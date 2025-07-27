3f005ba747ec7943fa6044beb14084049440a4e7
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-28 02:29:17 +0400

    Modification of "getConnection()" method in DbConnectionUtil

--- src/main/java/org/pronet/app/utils/DbConnectionUtil.java
+++ src/main/java/org/pronet/app/utils/DbConnectionUtil.java
@@ -7,9 +7,10 @@ import java.sql.SQLException;
 public class DbConnectionUtil {
     public static Connection getConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection("jdbc:mysql://localhost:3306" +
                     "/onlinedu_db?user=root&password=23042002&characterEncoding=UTF-8");
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
