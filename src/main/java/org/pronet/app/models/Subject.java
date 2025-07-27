1e3bda732a477f0b32a46e9593001fa398da7688
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-27 23:51:31 +0400

    Creation of "Subject" model

---
+++ src/main/java/org/pronet/app/models/Subject.java
@@ -0,0 +1,35 @@
+package org.pronet.app.models;
+
+import org.pronet.app.abstracts.BaseModel;
+
+public class Subject extends BaseModel {
+    private String name;
+    private Long teacherId;
+
+    public Subject() {
+    }
+
+    public Subject(Long id, String name, Long teacherId) {
+        super(id);
+        this.name = name;
+        this.teacherId = teacherId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Subject setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Long getTeacherId() {
+        return teacherId;
+    }
+
+    public Subject setTeacherId(Long teacherId) {
+        this.teacherId = teacherId;
+        return this;
+    }
+}
