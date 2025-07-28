5bb8eb488dc5fb8843037e7115432fc368bdc811
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-28 15:12:48 +0400

    Add new "getLongValue()" method to ParameterUtil

--- src/main/java/org/pronet/app/utils/ParameterUtil.java
+++ src/main/java/org/pronet/app/utils/ParameterUtil.java
@@ -1,8 +1,13 @@
 package org.pronet.app.utils;
 
 public class ParameterUtil {
-    public static String getValue(String parameter) {
+    public static String getStringValue(String parameter) {
         if (parameter != null) return parameter;
         return "";
+    }
+
+    public static long getLongValue(String parameter) {
+        if (parameter != null && !parameter.trim().isEmpty()) return Long.parseLong(parameter);
+        return 0;
     }
 }
