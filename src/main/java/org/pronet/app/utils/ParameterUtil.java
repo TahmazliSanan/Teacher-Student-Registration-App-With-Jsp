6c397581fc99ebdeb2ea2785a84e895400f2981d
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-28 14:17:25 +0400

    Creation of ParameterUtil

---
+++ src/main/java/org/pronet/app/utils/ParameterUtil.java
@@ -0,0 +1,8 @@
+package org.pronet.app.utils;
+
+public class ParameterUtil {
+    public static String getValue(String parameter) {
+        if (parameter != null) return parameter;
+        return "";
+    }
+}
