4c887f1be2fb62e23e60758b1083b5506b7398c0
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2025-07-29 00:40:43 +0400

    Create new fields and overload constructor

--- src/main/java/org/pronet/app/models/Subject.java
+++ src/main/java/org/pronet/app/models/Subject.java
@@ -4,9 +4,18 @@ import org.pronet.app.abstracts.BaseModel;
 
 public class Subject extends BaseModel {
     private String name;
+    private String teacherFirstName;
+    private String teacherLastName;
     private Long teacherId;
 
     public Subject() {
+    }
+
+    public Subject(Long id, String name, String teacherFirstName, String teacherLastName) {
+        super(id);
+        this.name = name;
+        this.teacherFirstName = teacherFirstName;
+        this.teacherLastName = teacherLastName;
     }
 
     public Subject(String name, Long teacherId) {
@@ -26,6 +35,24 @@ public class Subject extends BaseModel {
 
     public Subject setName(String name) {
         this.name = name;
+        return this;
+    }
+
+    public String getTeacherFirstName() {
+        return teacherFirstName;
+    }
+
+    public Subject setTeacherFirstName(String teacherFirstName) {
+        this.teacherFirstName = teacherFirstName;
+        return this;
+    }
+
+    public String getTeacherLastName() {
+        return teacherLastName;
+    }
+
+    public Subject setTeacherLastName(String teacherLastName) {
+        this.teacherLastName = teacherLastName;
         return this;
     }
 
